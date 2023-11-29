745471b24dc8c0260302033c29d6293e25e14c7c
Erik Öberg <erik.oberg@miun.se>
2023-11-29 12:03:38 +0100

    Nya yatzyspelet

---
+++ YatzyGame/GameEngine.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YatzyGame
+{
+    class GameEngine
+    {
+        #region public properties
+        public int ScoreOnes => _scores[0];
+        public int ScoreTwos => _scores[1];
+        public bool CanRollDice => _countRolls <= 3; 
+        public int Total => _scores.Sum();
+        #endregion
+        #region Private fields
+        private int _countRolls = 1;
+        private readonly Random _random = new();
+        private int[] _dice = new int[5];
+        private bool[] _usedCategories = { false, false, false, false, false, false };
+        private int[] _scores = new int[6];
+        private bool[] _savedDice = { false, false, false, false, false }; 
+        #endregion
+        #region Tärningar
+        /// <summary>
+        /// Kastar fem tärningar
+        /// </summary>
+        /// <returns>array med fem fack, heltal</returns>
+        public int[] RollDice()
+        {
+            for (int i = 0; i < _dice.Length; i++)
+            {
+                // undersök om tärningen är sparad eller inte
+                if (!_savedDice[i])
+                {
+                    _dice[i] = _random.Next(1, 7);
+                }
+            }
+            _countRolls++;
+            return _dice;
+        }
+
+        /// <summary>
+        /// Kastar fem tärningar men sparar vissa
+        /// </summary>
+        /// <returns>array med fem fack, heltal</returns>
+        public int[] RollDice(bool[] saved)
+        {
+            _savedDice = saved;
+            return RollDice();
+        } 
+        #endregion
+
+        public bool BetOnCategory(int category)
+        {
+            // är kategorin tillgänglig?
+            if (_usedCategories[category - 1])
+                return false;
+            _countRolls = 1;
+
+          CalculateScore(category);
+            return true;
+        }
+
+        private int CalculateScore(int category)
+        {
+            int score = 0;
+            foreach (int die in _dice) { 
+               if(die == category)
+                {
+                    score += die;
+                }
+            }
+            _scores[category-1] = score;
+            _usedCategories[category-1] = true;
+            return score;
+        }
+    }
+}
---
+++ YatzyGame/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YatzyGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GameEngine _game;
+        int[] _dice;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _game = new GameEngine();
+        }
+
+        private void btnRollDice_Click(object sender, RoutedEventArgs e)
+        {
+            // Ta reda på. Vilka tärningar har vi sparat?
+            bool[] saved = new bool[]
+            {
+                (bool)chkDiceOne.IsChecked,
+                (bool)chkDiceTwo.IsChecked,
+                (bool)chkDiceThree.IsChecked,
+                (bool)chkDiceFour.IsChecked,
+                (bool)chkDiceFive.IsChecked,
+            };
+            _dice = _game.RollDice(saved);
+            DisplayDice();
+            CanRollDice();
+        }
+
+        private void DisplayDice()
+        {
+            diceOne.Content = _dice[0];
+            diceTwo.Content = _dice[1];
+            diceThree.Content =_dice[2];
+            diceFour.Content = _dice[3];
+            diceFive.Content = _dice[4];
+        }
+
+        private void DisplayScores()
+        {
+            txtOnes.Text = _game.ScoreOnes.ToString();
+            txtTwos.Text = _game.ScoreTwos.ToString();
+
+        }
+
+        private void BetOnOnes(object sender, MouseButtonEventArgs e)
+        {
+            _game.BetOnCategory(1);
+            txtOnes.Background = Brushes.Tan;
+        }
+
+        private void BetOnCategory(object sender, MouseButtonEventArgs e)
+        {
+            Label selected = sender as Label;
+            string tag = selected.Tag.ToString();
+            bool isSuccessful = _game.BetOnCategory(int.Parse(tag));
+            if (!isSuccessful)
+            {
+                MessageBox.Show("Du får inte spara i samma kategori flera gånger.");
+            }
+            DisplayScores();
+            CanRollDice();
+        }
+        private void CanRollDice()
+        {
+            btnRollDice.IsEnabled = _game.CanRollDice;
+
+        }
+    }
+}
